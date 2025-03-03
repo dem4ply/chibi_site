36fbe06122d74395282868efc507f1f7765756e8
dem4ply <dem4ply@gmail.com>
2025-03-03 01:47:46 -0600

    iniciando repo chibi_site

---
+++ tests/test_chibi_site.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+
+
+class Test_chibi_site( unittest.TestCase ):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_should_work(self):
+        pass
