659ff9b249be2bd0c6c1788d30e354b077358736
dem4ply <dem4ply@gmail.com>
2025-03-04 10:52:51 -0600

    prototipo de chibi_site

--- chibi_site/chibi_site.py
+++ chibi_site/chibi_site.py
@@ -1,1 +1,43 @@
 # -*- coding: utf-8 -*-
+from chibi_requests import Chibi_url
+from chibi_requests.response import Response as Response_base
+
+
+class Response( Response_base ):
+    is_raise_when_no_ok = True
+
+
+class Chibi_site( Chibi_url ):
+    def __new__( cls, *args, **kw ):
+        kw.setdefault( 'response_class', Response )
+        obj = super().__new__( cls, *args, **kw )
+
+        return obj
+
+    @property
+    def response( self ):
+        try:
+            return self._response
+        except AttributeError:
+            self._response = self.get()
+            return self._response
+
+    @property
+    def soup( self ):
+        return self.response.native
+
+    @property
+    def images( self ):
+        return self.soup.find_all( 'img' )
+
+    @property
+    def sections( self ):
+        return self.soup.find_all( 'section' )
+
+    @property
+    def links( self ):
+        return self.soup.find_all( 'a' )
+
+    @property
+    def links_as_string( self ):
+        return list( a.attrs[ 'href' ] for a in self.links )
--- tests/test_chibi_site.py
+++ tests/test_chibi_site.py
@@ -1,14 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import unittest
+from chibi_site import Chibi_site
+from vcr_unittest import VCRTestCase
+from bs4 import BeautifulSoup
 
 
 class Test_chibi_site( unittest.TestCase ):
-    def setUp(self):
+    def setUp( self ):
         pass
 
-    def tearDown(self):
+    def tearDown( self ):
         pass
 
-    def test_should_work(self):
-        pass
+    def test_should_work( self ):
+        Chibi_site( 'https://danbooru.donmai.us/' )
+
+
+class Test_danbooru( VCRTestCase ):
+    def setUp( self ):
+        super().setUp()
+        self.site = Chibi_site( 'https://danbooru.donmai.us/' )
+
+
+class Test_soup( Test_danbooru ):
+    def test_soup_should_work( self ):
+        self.assertTrue( self.site.soup )
+
+    def test_soup_should_be_instance_soup( self ):
+        self.assertIsInstance( self.site.soup, BeautifulSoup )
+
+
+class Test_soup_images( Test_danbooru ):
+    def test_soup_should_work( self ):
+        self.assertTrue( self.site.images )
+
+
+class Test_soup_sections( Test_danbooru ):
+    def test_soup_should_work( self ):
+        self.assertTrue( self.site.sections )
+
+
+class Test_soup_links( Test_danbooru ):
+    def test_soup_should_work( self ):
+        self.assertTrue( self.site.links )
+
+    def test_links_should_be_a_list( self ):
+        self.assertIsInstance( self.site.links, list )
+
+    def test_links_as_string_should_be_a_list( self ):
+        self.assertIsInstance( self.site.links_as_string, list )
+
+    def test_links_as_string_all_items_should_be_strings( self ):
+        self.assertTrue( self.site.links_as_string )
+        for link in self.site.links_as_string:
+            self.assertIsInstance( link, str )
