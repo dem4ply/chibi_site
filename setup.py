ca28370c6178deba6697e99c5f68e4180f83deb4
dem4ply <dem4ply@gmail.com>
2025-05-16 17:09:41 -0600

    Bump version: 0.0.3 → 0.1.0

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
     name='chibi_site',
     packages=find_packages(include=['chibi_site', 'chibi_site.*']),
     url='https://github.com/dem4ply/chibi_site',
-    version='0.0.3',
+    version='0.1.0',
     zip_safe=False,
 )
